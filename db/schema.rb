52b21b890bfe5414f539ba7be0e3cb52ea0a0548
jobmassaro <jobmassaro@gmai.com>
2016-08-10 15:51:20 +0200

    add

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810085024) do
+ActiveRecord::Schema.define(version: 20160810131953) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "titolo"
+    t.text     "descrizione"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
